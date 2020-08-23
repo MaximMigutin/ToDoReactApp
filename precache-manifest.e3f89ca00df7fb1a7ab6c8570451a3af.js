981c6d27b7b480d9a74dabc6b67a187a2e17f6cd
Maxim Migutin <62377399+mmigutin@users.noreply.github.com>
2020-08-23 23:26:26 +0300

    Updates

---
+++ precache-manifest.e3f89ca00df7fb1a7ab6c8570451a3af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e9d70216432ac63880614b25076519",
+    "url": "/TodoReactApp/index.html"
+  },
+  {
+    "revision": "10acb378d0df99bde5a3",
+    "url": "/TodoReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d8440651451cde44124",
+    "url": "/TodoReactApp/static/js/2.af02aeb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoReactApp/static/js/2.af02aeb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10acb378d0df99bde5a3",
+    "url": "/TodoReactApp/static/js/main.bc26ab55.chunk.js"
+  },
+  {
+    "revision": "ab96d7bd9908d603ae0c",
+    "url": "/TodoReactApp/static/js/runtime-main.7590630c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
